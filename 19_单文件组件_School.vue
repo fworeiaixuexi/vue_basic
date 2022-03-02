cd1446edfcae7ad92a2aad084f813bb8267a556e
fworeiaixuexi <74755055+fworeiaixuexi@users.noreply.github.com>
2022-03-02 17:10:46 +0800

    Create 19_单文件组件_School.vue

---
+++ 19_单文件组件_School.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="demo">
+    <h2>学校名称：{{name}}</h2>
+    <h2>学校地址：{{address}}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'School',
+    data(){
+      return {
+        name:'尚硅谷',
+        address:'北京'
+      }
+    },
+    methods:{
+      showName(){
+        alert(this.name)
+      }
+    }
+  }
+  // export default school //默认暴露，要暴露后别的才能使用这个组件
+</script>
+
+<style>
+  .demo{
+    background-color: orange;
+  }
+</style>
